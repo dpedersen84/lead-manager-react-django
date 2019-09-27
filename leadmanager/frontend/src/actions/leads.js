fa6eba916e4c6560dca2bf5430bb9a1e1614f178
Dan <dpedersen84@gmail.com>
2019-09-27 11:05:14 -0500

    add lead, form component, reducer, action, type to add lead

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { GET_LEADS, DELETE_LEAD } from "./types";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 // Get Leads
 export const getLeads = () => dispatch => {
@@ -15,7 +15,7 @@ export const getLeads = () => dispatch => {
     .catch(err => console.log(err));
 };
 
-// Delete Leads
+// Delete Lead
 export const deleteLead = id => dispatch => {
   axios
     .delete(`/api/leads/${id}/`)
@@ -23,6 +23,19 @@ export const deleteLead = id => dispatch => {
       dispatch({
         type: DELETE_LEAD,
         payload: id
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+// Add Lead
+export const addLead = lead => dispatch => {
+  axios
+    .post("/api/leads/", lead)
+    .then(res => {
+      dispatch({
+        type: ADD_LEAD,
+        payload: res.data
       });
     })
     .catch(err => console.log(err));
